77c9c55716468612b55f036ce512785343bab6c9
Hugo Costa <costarafaelhugo@gmail.com>
2021-11-30 10:01:18 -0300

    corrigi o bug que ocorria ao girar a tela

--- app/src/main/java/com/hugorafaelcosta/myapplication/MainActivity.kt
+++ app/src/main/java/com/hugorafaelcosta/myapplication/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.hugorafaelcosta.myapplication
 
 import android.annotation.SuppressLint
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -21,11 +22,20 @@ class MainActivity : AppCompatActivity() {
     internal lateinit var countDownTimer: CountDownTimer
     internal val initialCountDown: Long = 60000
     internal val countDownInterval: Long = 1000
+    internal var timeLeftOnTimer: Long = 1000
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+        private const val SCORE_KEY = "SCORE_KEY"
+        private const val TIME_LEFT_KEY = "TIME_LEFT_KEY"
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate called. Score is: $score")
 
         tapMeButton = findViewById(R.id.TapMeButton)
         gameScoreTextView = findViewById(R.id.gameScoreTextView)
@@ -35,10 +45,32 @@ class MainActivity : AppCompatActivity() {
             incrementScore()
         }
 
-     resetGame()
+        if (savedInstanceState != null) {
+            score = savedInstanceState.getInt(SCORE_KEY)
+            timeLeftOnTimer = savedInstanceState.getLong(TIME_LEFT_KEY)
+            restoreGame()
+        }else{
+            resetGame()
+        }
     }
 
-    private fun resetGame(){
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt(SCORE_KEY, score)
+        outState.putLong(TIME_LEFT_KEY, timeLeftOnTimer)
+        countDownTimer.cancel()
+
+        Log.d(TAG, "onSaveInstanceState: Saving Score: $score e Time Left: $timeLeftOnTimer")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy called.")
+
+    }
+
+    private fun resetGame() {
         score = 0
 
         gameScoreTextView.text = getString(R.string.playerScore, score)
@@ -46,22 +78,44 @@ class MainActivity : AppCompatActivity() {
         val initialTimeLeft = initialCountDown / 1000
         timeTextView.text = getString(R.string.playerTime, initialTimeLeft)
 
-        countDownTimer = object : CountDownTimer (initialCountDown, countDownInterval) {
+        countDownTimer = object : CountDownTimer(initialCountDown, countDownInterval) {
             override fun onTick(millisUntilFinished: Long) {
+                timeLeftOnTimer = millisUntilFinished
                 val timeLeft = millisUntilFinished / 1000
                 timeTextView.text = getString(R.string.playerTime, timeLeft)
             }
 
             override fun onFinish() {
-             endGame()
+                endGame()
             }
         }
         gameStarted = false
     }
 
+    private fun restoreGame(){
+        gameScoreTextView.text = getString(R.string.playerScore, score)
+
+        val restoredTime = timeLeftOnTimer /1000
+        timeTextView.text = getString(R.string.playerTime, restoredTime)
+
+        countDownTimer = object : CountDownTimer(timeLeftOnTimer, countDownInterval) {
+            override fun onTick(millisUntilFinished: Long) {
+                timeLeftOnTimer = millisUntilFinished
+                val timeLeft = millisUntilFinished / 1000
+                timeTextView.text = getString(R.string.playerTime, timeLeft)
+            }
+
+            override fun onFinish() {
+                endGame()
+            }
+        }
+        countDownTimer.start()
+        gameStarted = true
+    }
+
     @SuppressLint("StringFormatInvalid")
     private fun incrementScore() {
-        if(!gameStarted){
+        if (!gameStarted) {
             startGame()
         }
         score += 1
@@ -75,7 +129,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun endGame() {
-        Toast.makeText(this, getString(R.string.gameOverMessage,score), Toast.LENGTH_LONG).show()
+        Toast.makeText(this, getString(R.string.gameOverMessage, score), Toast.LENGTH_LONG).show()
         resetGame()
     }
 }
